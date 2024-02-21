1bae402a8ed314937d44d62d0146029fc1a385fc
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-02-21 16:44:51 +0100

    [add] js logic for random cab number and CP code

--- js/script.js
+++ js/script.js
@@ -2,17 +2,20 @@
 const btnGenerator = document.querySelector(".btn_generator");
 
 //Form variables
-const NameInput = document.querySelector(".input-name");
-const kmInput = document.querySelector(".input-km");
-const ageSelected = document.querySelector(".selected-age");
+const NameInput = document.getElementById("input-name");
+const kmInput = document.getElementById("input-km");
+const ageSelected = document.getElementById("selected-age");
 
 //Other variables
 const priceKm = 0.21;
 const discountMinors = 0.2;
 const discountOver = 0.4;
+const ticketSection = document.getElementById("ticket-recap");
 const nameTicket = document.querySelector(".name_ticket");
 let typeTicket = document.querySelector(".type_ticket");
 let priceTicket = document.querySelector(".price_ticket");
+let cabNumber = document.querySelector(".cab_number");
+let cpCode = document.querySelector(".CP_code");
 let price;
 let typeMessage = "Biglietto standard";
 
@@ -38,5 +41,9 @@ btnGenerator.addEventListener("click", function (event) {
 
   nameTicket.innerHTML = NameInput.value;
   typeTicket.innerHTML = typeMessage;
-  priceTicket.innerHTML += " " + price.toFixed(2);
+  cabNumber.innerHTML = Math.floor(Math.random() * 20) + 1;
+  cpCode.innerHTML = Math.floor(Math.random() * 90000) + 10000;
+  priceTicket.innerHTML = `${price.toFixed(2)} €`;
+
+  ticketSection.classList.remove("d-none");
 });
